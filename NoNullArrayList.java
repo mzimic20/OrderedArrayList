0bbb9cb204585fc202e9d3af69a4e48d0064193b
mzimic20 <>
2020-12-10 09:19:00 -0500

    all methods in NoNullArrayList work

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -11,17 +11,17 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
   public boolean add(T t) {
-      if (t == null) throw new IllegalArgumentException("IllegalArgumentException: " + t + " cannot be null");
+      if (t == null) throw new IllegalArgumentException("IllegalArgumentException: E for add(E) cannot be null");
       else return super.add(t);
   }
 
   public void add(int index, T t) {
-    if (t == null) throw new IllegalArgumentException("IllegalArgumentException: " + t + " cannot be null");
+    if (t == null) throw new IllegalArgumentException("IllegalArgumentException: E for add(index,E) cannot be null");
     else super.add(index, t);
   }
 
   public T set(int index, T t) {
-    if (t == null) throw new IllegalArgumentException("IllegalArgumentException: " + t + " cannot be null");
+    if (t == null) throw new IllegalArgumentException("IllegalArgumentException: E for set(index,E) cannot be null");
     else return super.set(index, t);
   }
 
--- Tester.java
+++ Tester.java
@@ -1,8 +1,20 @@
 public class Tester {
 
   public static void main(String[]args) {
-    NoNullArrayList a = new NoNullArrayList();
-    NoNullArrayList b = new NoNullArrayList(15);
+    NoNullArrayList<String> a = new NoNullArrayList<String>();
+    NoNullArrayList<String> b = new NoNullArrayList<String>(15);
+
+    System.out.println("a: " + a );
+    System.out.println("b: " + b );
+
+    a.add("bok");
+    a.add("kako si");
+//    a.add(null);
+    a.set(0, "jedi hranu sine");
+    b.add("zdravo");
+    b.add("idem domu");
+//    b.add(null);
+    b.set(0, "necu vise baba");
 
     System.out.println("a: " + a );
     System.out.println("b: " + b );
